128f20ffdbc66197308a8b4f7d8e31ed041405f4
nikhil.lingam <nikhi013@gmail.com>
2021-12-07 12:55:37 +0530

    removing lombok dependency

--- ecom-service/src/main/java/com/ecom/controller/PersonController.java
+++ ecom-service/src/main/java/com/ecom/controller/PersonController.java
@@ -3,12 +3,14 @@ package com.ecom.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.print.attribute.standard.Media;
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,11 +29,11 @@ public class PersonController {
 	@Autowired
 	private PersonDAO personDAOImpl;
 
-	private final Logger log = LoggerFactory.getLogger(PersonController.class);
+	private final Logger LOGGER = LoggerFactory.getLogger(PersonController.class);
 	
-	@PostMapping(value = "/persons")
-	public ResponseEntity<Person> createPerson(@RequestBody Person person) {
-		log.info("Inside createPerson method from PersonController");
+	@PostMapping(value = "/persons", headers = {"content-type=application/json"}, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Person> createPerson(@Valid @RequestBody Person person) {
+		LOGGER.info("Inside createPerson method from PersonController");
 		Person p = personDAOImpl.create(person);
 		if(p==null) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -41,7 +43,7 @@ public class PersonController {
 
 	@GetMapping(value = "/persons/{id}")
 	public ResponseEntity<Person> getPerson(@PathVariable Long id){
-		log.info("Inside getPerson method from PersonController");
+		LOGGER.info("Inside getPerson method from PersonController");
 		Optional<Person> optional = personDAOImpl.getPerson(id);
 		if(optional.isPresent()) {
 			return new ResponseEntity<>(optional.get(), HttpStatus.OK);
@@ -51,20 +53,20 @@ public class PersonController {
 	
 	@GetMapping(value = "/persons")
 	public ResponseEntity<List<Person>> getAllPersons(){
-		log.info("Inside getAllPersons method from PersonController");
+		LOGGER.info("Inside getAllPersons method from PersonController");
 		return new ResponseEntity<>(personDAOImpl.findAllPersons(), HttpStatus.OK);
 	}
 	
 	@DeleteMapping(value = "/persons/{id}")
 	public ResponseEntity<HttpStatus> deletePerson(@PathVariable Long id){
-		log.info("Inside deletePerson method from PersonController");
+		LOGGER.info("Inside deletePerson method from PersonController");
 		personDAOImpl.deletePerson(id);
 		return new ResponseEntity<>(HttpStatus.ACCEPTED);
 	}
 	
 	@PutMapping(value = "/persons/{id}")
 	public ResponseEntity<Person> updatePerson(@Valid @RequestBody Person person){
-		log.info("Inside updatePerson method from PersonController");
+		LOGGER.info("Inside updatePerson method from PersonController");
 		return new ResponseEntity<>(personDAOImpl.updatePerson(person), HttpStatus.OK);
 	}
 }
